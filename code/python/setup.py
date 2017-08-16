abb50a2786f2ad6b12742384f1a15e16b6571d2a
ashton <ashton.webster@gmail.com>
2017-08-16 15:02:07 -0400

    adds ability to install TCA as a pip package

---
+++ code/python/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='da_tool',
+      version='0.1',
+      description='TCA implementation',
+      url='https://github.com/jindongwang/transferlearning',
+      author='jindongwang',
+      author_email='jindongwang@outlook.com',
+      license='None',
+      packages=['da_tool'],
+      zip_safe=False)
